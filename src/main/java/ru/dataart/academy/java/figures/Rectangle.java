cf439546869c829ab9b085cab5fc8ed5ce6c720e
vladimir-gaydamakin <86650413+vladimir-gaydamakin@users.noreply.github.com>
2021-10-16 17:33:25 +0300

    Update Rectangle.java

--- src/main/java/ru/dataart/academy/java/figures/Rectangle.java
+++ src/main/java/ru/dataart/academy/java/figures/Rectangle.java
@@ -1,8 +1,8 @@
 package ru.dataart.academy.java.figures;
 
 public class Rectangle extends Figure {
-    private double width;
-    private double length;
+    private final double width;
+    private final double length;
 
     public Rectangle(double length, double width) {
         this.width = width;
